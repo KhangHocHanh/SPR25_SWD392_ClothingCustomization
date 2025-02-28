033fcee7822edeab62d99ee2940a1d8039d0a878
hai300420 <haidoi3004@gmail.com>
2025-03-01 05:08:01 +0700

    Fix File

---
+++ 3-Repository/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Model;
+using Repository.IRepository;
+
+namespace Repository.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository() { }
+
+        public UserRepository(ClothesCusShopContext context) => _context = context;
+    }
+}
