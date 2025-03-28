dd81b3c6f1f9e51bafcd9a57bd6c6967ef0f7bbf
Nhan Nguyen <hoangnhan13072003@gmail.com>
2025-03-28 18:54:58 +0700

    Update Code

--- 4-BusinessObject/RequestDTO/RequestDTO.cs
+++ 4-BusinessObject/RequestDTO/RequestDTO.cs
@@ -55,9 +55,31 @@ namespace BusinessObject.RequestDTO
             public DateTime? DeliveryDate { get; set; }
         }
         
-        public class ProductCreateDTO
+            public class ProductCreateDTO
+            {
+                [Required(ErrorMessage = "Category ID is required")]
+                public int CategoryId { get; set; }
+
+                [Required(ErrorMessage = "Product name is required")]
+                [StringLength(100, ErrorMessage = "Product name cannot exceed 100 characters")]
+                public string ProductName { get; set; } = null!;
+
+                [Required(ErrorMessage = "Price is required")]
+                [Range(1, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+   
+            public decimal Price { get; set; }
+
+            [Required(ErrorMessage = "Stock is required")]
+            [Range(1, int.MaxValue, ErrorMessage = "Stock must be at least 1")]
+            public int StockInStorage { get; set; }
+
+                public string? Image { get; set; }
+                public string? Description { get; set; }
+            }
+        public class ProductUpdateDTO
         {
-            [Required(ErrorMessage = "Category ID is required")]
+            public int ProductId { get; set; }
+
             public int CategoryId { get; set; }
 
             [Required(ErrorMessage = "Product name is required")]
@@ -65,27 +87,18 @@ namespace BusinessObject.RequestDTO
             public string ProductName { get; set; } = null!;
 
             [Required(ErrorMessage = "Price is required")]
-            [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+            [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
             public decimal Price { get; set; }
 
             [Required(ErrorMessage = "Stock is required")]
-            [Range(0, int.MaxValue, ErrorMessage = "Stock must be greater than 0")]
+            [Range(1, int.MaxValue, ErrorMessage = "Stock must be at least 1")]
             public int StockInStorage { get; set; }
 
-            public string? Image { get; set; }
-            public string? Description { get; set; }
-        }
-        public class ProductUpdateDTO
-        {
-            public int ProductId { get; set; } 
-            public int CategoryId { get; set; }
-            public string ProductName { get; set; } = null!;
-            public decimal Price { get; set; }
-            public int StockInStorage { get; set; }
             public string? Image { get; set; }
             public string? Description { get; set; }
             public bool IsDeleted { get; set; }
         }
+
         public class CategoryCreateDTO
         {
             [Required]
